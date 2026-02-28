bde309283173207979d55b108dfaf213bced1e3b
Rohit Vishwakarma <rohitvishwakarma4800@gmail.com>
2026-02-28 13:49:57 +0530

    Create ARRAY.C

---
+++ ARRAY.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+  int a[10],pos,n=8, element,i;
+  printf("enter the array element");
+  for(int i=0;i<n;i++) {
+      scanf("%d",&a[1]);
+    }
+    printf("enter the position");
+    scanf("%d",&pos);
+    printf("enter the element");
+    scanf("%d",&element);
+    
+    return 0;
+}
